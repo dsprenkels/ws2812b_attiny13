e8efaea0c1f92702470bfd26e262d571f78030e7
Amber Sprenkels <hello@dsprenkels.com>
2018-12-10 00:30:30 +0100

    Cleanup: r16 was not used

--- ws2812b_attiny13.c
+++ ws2812b_attiny13.c
@@ -12,7 +12,7 @@ void ws2812b_set_color_no_reset(const uint8_t pin_value, const uint8_t red, cons
                 //   - 4 if a zero bit is transmitted
                 //
                 // In the spare time we have left, we read out the next bit from the current color
-                // byte. We keep a mask bit in r18, do a bitwise and operation, and branch if (not)
+                // byte. We keep a mask bit in r17, do a bitwise and operation, and branch if (not)
                 // zero. After 8 shifts the carry flag will be set and we will move on to the next
                 // color bit.
                 //
@@ -22,9 +22,9 @@ void ws2812b_set_color_no_reset(const uint8_t pin_value, const uint8_t red, cons
                 // take a lot of effort to find a good argument on why this should be impossible.
                 //
                 ".green:                        \n\t"
-                "mov r17,%[green]               \n\t"
-                "ldi r18,0x80                   \n\t"
-                "and r17,r18                    \n\t"
+                "mov r16,%[green]               \n\t"
+                "ldi r17,0x80                   \n\t"
+                "and r16,r17                    \n\t"
                 "brne .green_transmit_one_0     \n\t"
                 "rjmp .green_transmit_zero_0    \n\t"
                 
@@ -34,41 +34,41 @@ void ws2812b_set_color_no_reset(const uint8_t pin_value, const uint8_t red, cons
                 ".green_transmit_one_0:         \n\t"
                 "out 0x18,%[hi]                 \n\t" // cycle 0
                 "nop                            \n\t" // cycle 1
-                "lsr r18                        \n\t" // cycle 2
+                "lsr r17                        \n\t" // cycle 2
                 "brcs .green_transmit_one_done  \n\t" // cycle 3
                 "rjmp .+0                       \n\t" // cycle 4
-                "mov r17,%[green]               \n\t" // cycle 6
-                "and r17,r18                    \n\t" // cycle 7
+                "mov r16,%[green]               \n\t" // cycle 6
+                "and r16,r17                    \n\t" // cycle 7
                 "out 0x18,%[lo]                 \n\t" // cycle 8
                 "brne .green_transmit_one_1     \n\t" // cycle 9
                 "rjmp .green_transmit_zero_0    \n\t" // cycle 10
                 
                 ".green_transmit_one_done:      \n\t"
-                "ldi r18,0x80                   \n\t" // cycle 5
-                "mov r17,%[red]                 \n\t" // cycle 6
-                "and r17,r18                    \n\t" // cycle 7
+                "ldi r17,0x80                   \n\t" // cycle 5
+                "mov r16,%[red]                 \n\t" // cycle 6
+                "and r16,r17                    \n\t" // cycle 7
                 "out 0x18,%[lo]                 \n\t" // cycle 8
                 "brne .red_transmit_one_1       \n\t" // cycle 9
                 "rjmp .red_transmit_zero_0      \n\t" // cycle 10
                 
                 ".green_transmit_zero_0:        \n\t"
                 "out 0x18,%[hi]                 \n\t" // cycle 0
-                "lsr r18                        \n\t" // cycle 1
+                "lsr r17                        \n\t" // cycle 1
                 "brcs .green_transmit_zero_done \n\t" // cycle 2
                 "nop                            \n\t" // cycle 3
                 "out 0x18,%[lo]                 \n\t" // cycle 4
                 "rjmp .+0                       \n\t" // cycle 5
-                "mov r17,%[green]               \n\t" // cycle 7
-                "and r17,r18                    \n\t" // cycle 8
+                "mov r16,%[green]               \n\t" // cycle 7
+                "and r16,r17                    \n\t" // cycle 8
                 "brne .green_transmit_one_1     \n\t" // cycle 9
                 "rjmp .green_transmit_zero_0    \n\t" // cycle 10
                 
                 ".green_transmit_zero_done:     \n\t"
                 "out 0x18,%[lo]                 \n\t" // cycle 4
                 "nop                            \n\t" // cycle 5
-                "ldi r18,0x80                   \n\t" // cycle 6
-                "mov r17,%[red]                 \n\t" // cycle 7
-                "and r17,r18                    \n\t" // cycle 8
+                "ldi r17,0x80                   \n\t" // cycle 6
+                "mov r16,%[red]                 \n\t" // cycle 7
+                "and r16,r17                    \n\t" // cycle 8
                 "brne .red_transmit_one_1       \n\t" // cycle 9
                 "rjmp .red_transmit_zero_0      \n\t" // cycle 10
                 
@@ -78,41 +78,41 @@ void ws2812b_set_color_no_reset(const uint8_t pin_value, const uint8_t red, cons
                 ".red_transmit_one_0:           \n\t"
                 "out 0x18,%[hi]                 \n\t" // cycle 0
                 "nop                            \n\t" // cycle 1
-                "lsr r18                        \n\t" // cycle 2
+                "lsr r17                        \n\t" // cycle 2
                 "brcs .red_transmit_one_done    \n\t" // cycle 3
                 "rjmp .+0                       \n\t" // cycle 4
-                "mov r17,%[red]                 \n\t" // cycle 6
-                "and r17,r18                    \n\t" // cycle 7
+                "mov r16,%[red]                 \n\t" // cycle 6
+                "and r16,r17                    \n\t" // cycle 7
                 "out 0x18,%[lo]                 \n\t" // cycle 8
                 "brne .red_transmit_one_1       \n\t" // cycle 9
                 "rjmp .red_transmit_zero_0      \n\t" // cycle 10
                 
                 ".red_transmit_one_done:        \n\t"
-                "ldi r18,0x80                   \n\t" // cycle 5
-                "mov r17,%[blue]                \n\t" // cycle 6
-                "and r17,r18                    \n\t" // cycle 7
+                "ldi r17,0x80                   \n\t" // cycle 5
+                "mov r16,%[blue]                \n\t" // cycle 6
+                "and r16,r17                    \n\t" // cycle 7
                 "out 0x18,%[lo]                 \n\t" // cycle 8
                 "brne .blue_transmit_one_1      \n\t" // cycle 9
                 "rjmp .blue_transmit_zero_0     \n\t" // cycle 10
                 
                 ".red_transmit_zero_0:          \n\t"
                 "out 0x18,%[hi]                 \n\t" // cycle 0
-                "lsr r18                        \n\t" // cycle 1
+                "lsr r17                        \n\t" // cycle 1
                 "brcs .red_transmit_zero_done   \n\t" // cycle 2
                 "nop                            \n\t" // cycle 3
                 "out 0x18,%[lo]                 \n\t" // cycle 4
                 "rjmp .+0                       \n\t" // cycle 5
-                "mov r17,%[red]                 \n\t" // cycle 7
-                "and r17,r18                    \n\t" // cycle 8
+                "mov r16,%[red]                 \n\t" // cycle 7
+                "and r16,r17                    \n\t" // cycle 8
                 "brne .red_transmit_one_1       \n\t" // cycle 9
                 "rjmp .red_transmit_zero_0      \n\t" // cycle 10
                 
                 ".red_transmit_zero_done:       \n\t"
                 "out 0x18,%[lo]                 \n\t" // cycle 4
                 "nop                            \n\t" // cycle 5
-                "ldi r18,0x80                   \n\t" // cycle 6
-                "mov r17,%[blue]                \n\t" // cycle 7
-                "and r17,r18                    \n\t" // cycle 8
+                "ldi r17,0x80                   \n\t" // cycle 6
+                "mov r16,%[blue]                \n\t" // cycle 7
+                "and r16,r17                    \n\t" // cycle 8
                 "brne .blue_transmit_one_1      \n\t" // cycle 9
                 "rjmp .blue_transmit_zero_0     \n\t" // cycle 10
                 
@@ -122,11 +122,11 @@ void ws2812b_set_color_no_reset(const uint8_t pin_value, const uint8_t red, cons
                 ".blue_transmit_one_0:          \n\t"
                 "out 0x18,%[hi]                 \n\t" // cycle 0
                 "rjmp .+0                       \n\t" // cycle 1
-                "lsr r18                        \n\t" // cycle 3
+                "lsr r17                        \n\t" // cycle 3
                 "brcs .blue_transmit_one_done   \n\t" // cycle 4
                 "nop                            \n\t" // cycle 5
-                "mov r17,%[blue]                \n\t" // cycle 6
-                "and r17,r18                    \n\t" // cycle 7
+                "mov r16,%[blue]                \n\t" // cycle 6
+                "and r16,r17                    \n\t" // cycle 7
                 "out 0x18,%[lo]                 \n\t" // cycle 8
                 "brne .blue_transmit_one_1      \n\t" // cycle 9
                 "rjmp .blue_transmit_zero_0     \n\t" // cycle 10
@@ -136,13 +136,13 @@ void ws2812b_set_color_no_reset(const uint8_t pin_value, const uint8_t red, cons
                 
                 ".blue_transmit_zero_0:         \n\t"
                 "out 0x18,%[hi]                 \n\t" // cycle 0
-                "lsr r18                        \n\t" // cycle 1
+                "lsr r17                        \n\t" // cycle 1
                 "brcs .blue_transmit_zero_done  \n\t" // cycle 2
                 "nop                            \n\t" // cycle 3
                 "out 0x18,%[lo]                 \n\t" // cycle 4
                 "rjmp .+0                       \n\t" // cycle 5
-                "mov r17,%[blue]                \n\t" // cycle 7
-                "and r17,r18                    \n\t" // cycle 8
+                "mov r16,%[blue]                \n\t" // cycle 7
+                "and r16,r17                    \n\t" // cycle 8
                 "brne .blue_transmit_one_1      \n\t" // cycle 9
                 "rjmp .blue_transmit_zero_0     \n\t" // cycle 10
                 
@@ -152,6 +152,6 @@ void ws2812b_set_color_no_reset(const uint8_t pin_value, const uint8_t red, cons
             
             : // No outputs
             : [lo] "r" (lo), [hi] "r" (hi), [green] "r" (green), [red] "r" (red), [blue] "r" (blue)
-            : "r16", "r17", "r18"
+            : "r16", "r17"
     );
 }
